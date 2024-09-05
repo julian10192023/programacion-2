115f5f3e0c8e004564cc367c75afd9711dc7a297
julian10192023 <142471561+julian10192023@users.noreply.github.com>
2024-09-05 00:34:22 -0500

    inicializar datos
    se inicializaron datos quemados para las funciones

--- co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
+++ co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
@@ -1,10 +1,83 @@
 package co.edu.uniquindio.biblioteca;
 
+import co.edu.uniquindio.biblioteca.model.Biblioteca;
+import co.edu.uniquindio.biblioteca.model.Libro;
+import co.edu.uniquindio.biblioteca.model.Miembro;
+
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        Biblioteca biblioteca = inicializarDatos();
+
     }
+
+    private static Biblioteca inicializarDatos() {
+        Biblioteca biblioteca = new Biblioteca();
+        Libro libro1 = new Libro();
+        libro1.setTitulo("CIEN AÑOS DE SOLEDAD");
+        libro1.setAutor("Gabriel García Márquez");
+        libro1.setIsbn("978-958-42-7450-2");
+        libro1.setPrestado(true);
+        Libro libro2 = new Libro();
+        libro2.setTitulo("El olvido que seremos");
+        libro2.setAutor("Héctor Abad Faciolince");
+        libro2.setIsbn(" 978-958-5457-60-6");
+        libro2.setPrestado(false);
+        Libro libro3 = new Libro();
+        libro3.setTitulo("Delirio");
+        libro3.setAutor("Laura Restrepo");
+        libro3.setIsbn("978-958-704-267-1");
+        libro3.setPrestado(false);
+        Libro libro4 = new Libro();
+        libro4.setTitulo("La vorágine");
+        libro4.setAutor("José Eustasio Rivera");
+        libro4.setIsbn("978-958-38-0215-3");
+        libro4.setPrestado(true);
+        Libro libro5 = new Libro();
+        libro5.setTitulo("Rosario Tijeras");
+        libro5.setAutor("Jorge Franco");
+        libro5.setIsbn("978-958-704-993-9");
+        libro5.setPrestado(true);
+
+        biblioteca.getListaLibros().add(libro1);
+        biblioteca.getListaLibros().add(libro2);
+        biblioteca.getListaLibros().add(libro3);
+        biblioteca.getListaLibros().add(libro4);
+        biblioteca.getListaLibros().add(libro5);
+
+        Miembro miembro1 = new Miembro();
+        miembro1.setNombre("julian");
+        miembro1.setId(1000192445);
+        miembro1.setEdad(25);
+        Miembro miembro2 = new Miembro();
+        miembro2.setNombre("andy");
+        miembro2.setId(1235445);
+        miembro2.setEdad(25);
+        Miembro miembro3 = new Miembro();
+        miembro3.setNombre("camilo");
+        miembro3.setId(867537909);
+        miembro3.setEdad(35);
+        Miembro miembro4 = new Miembro();
+        miembro4.setNombre("juliana");
+        miembro4.setId(123);
+        miembro4.setEdad(42);
+        Miembro miembro5 = new Miembro();
+        miembro5.setNombre("camila");
+        miembro5.setId(30975325);
+        miembro5.setEdad(50);
+
+        biblioteca.getListaMiembros().add(miembro1);
+        biblioteca.getListaMiembros().add(miembro2);
+        biblioteca.getListaMiembros().add(miembro3);
+        biblioteca.getListaMiembros().add(miembro4);
+        biblioteca.getListaMiembros().add(miembro5);
+
+        return biblioteca;
+    }
+
+
 }
--- co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/model/Biblioteca.java
+++ co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/model/Biblioteca.java
@@ -4,10 +4,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Biblioteca {
+    private String nombre;
     private List<Libro>listaLibros = new ArrayList();
     private  List<Miembro>listaMiembros = new ArrayList();
     private List<Prestamo>listaPrestamos = new ArrayList();
     private List<Empleado>listaEmpleados = new ArrayList();
     private List<Bibliotecario>listaBibliotecarios = new ArrayList();
+
+    public Biblioteca() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Libro> getListaLibros() {
+        return listaLibros;
+    }
+
+    public void setListaLibros(List<Libro> listaLibros) {
+        this.listaLibros = listaLibros;
+    }
+
+    public List<Miembro> getListaMiembros() {
+        return listaMiembros;
+    }
+
+    public void setListaMiembros(List<Miembro> listaMiembros) {
+        this.listaMiembros = listaMiembros;
+    }
+
+    public List<Prestamo> getListaPrestamos() {
+        return listaPrestamos;
+    }
+
+    public void setListaPrestamos(List<Prestamo> listaPrestamos) {
+        this.listaPrestamos = listaPrestamos;
+    }
+
+    public List<Empleado> getListaEmpleados() {
+        return listaEmpleados;
+    }
+
+    public void setListaEmpleados(List<Empleado> listaEmpleados) {
+        this.listaEmpleados = listaEmpleados;
+    }
+
+    public List<Bibliotecario> getListaBibliotecarios() {
+        return listaBibliotecarios;
+    }
+
+    public void setListaBibliotecarios(List<Bibliotecario> listaBibliotecarios) {
+        this.listaBibliotecarios = listaBibliotecarios;
+    }
 
 }
